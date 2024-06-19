9dbdec74d4d16958a17fe25e3ca711836653a4c4
unknown <pvhung97>
2024-06-19 22:31:10 +0900

    Fix driver service still validate browser name on start

--- src/main/java/io/kanthis/WinDriverCommandExecutor.java
+++ src/main/java/io/kanthis/WinDriverCommandExecutor.java
@@ -6,10 +6,16 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.net.URL;
 
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.remote.Command;
+import org.openqa.selenium.remote.CommandCodec;
 import org.openqa.selenium.remote.HttpCommandExecutor;
 import org.openqa.selenium.remote.Response;
+import org.openqa.selenium.remote.ResponseCodec;
+import org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec;
 import org.openqa.selenium.remote.http.ClientConfig;
+import org.openqa.selenium.remote.http.HttpRequest;
+import org.openqa.selenium.remote.http.HttpResponse;
 
 public class WinDriverCommandExecutor extends HttpCommandExecutor {
 
@@ -25,16 +31,32 @@ public class WinDriverCommandExecutor extends HttpCommandExecutor {
     public Response execute(Command command) throws IOException {
         Response response = super.execute(command);
         if (NEW_SESSION.equals(command.getName())) {
-            try {
-                Field commandCodec = null;
-                commandCodec = this.getClass().getSuperclass().getDeclaredField("commandCodec");
-                commandCodec.setAccessible(true);
-                commandCodec.set(this, new WinDriverCommandCodec());
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
+            setCommandCodec(new WinDriverCommandCodec());
+            setResponseCodec(new W3CHttpResponseCodec());
         }
         return response;
+    }
+
+    public void setCommandCodec(CommandCodec<HttpRequest> newCodec) {
+        try {
+            Field commandCodec = null;
+            commandCodec = HttpCommandExecutor.class.getDeclaredField("commandCodec");
+            commandCodec.setAccessible(true);
+            commandCodec.set(this, newCodec);
+        } catch (Exception ex) {
+            throw new WebDriverException(ex);
+        }
+    }
+
+    public void setResponseCodec(ResponseCodec<HttpResponse> codec) {
+        try {
+            Field responseCodec = null;
+            responseCodec = HttpCommandExecutor.class.getDeclaredField("responseCodec");
+            responseCodec.setAccessible(true);
+            responseCodec.set(this, codec);
+        } catch (Exception ex) {
+            throw new WebDriverException(ex);
+        }
     }
     
 }
--- src/main/java/io/kanthis/WinDriverService.java
+++ src/main/java/io/kanthis/WinDriverService.java
@@ -6,10 +6,17 @@ import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.os.CommandLine;
+import org.openqa.selenium.remote.service.DriverFinder;
 import org.openqa.selenium.remote.service.DriverService;
 
 import com.google.auto.service.AutoService;
@@ -18,6 +25,15 @@ public class WinDriverService extends DriverService {
 
     public static final String WINDRIVER_EXE_PROPERTY = "windriver.execute";
     public static final String WINDRIVER_NAME = "windriver";
+
+    private final ExecutorService waitAvailableExecutor = Executors.newFixedThreadPool(1, r -> {
+        Thread thread = new Thread(r);
+        thread.setName("Wait windriver available executor");
+        thread.setDaemon(true);
+        return thread;
+    });
+
+    private final ReentrantLock lock = new ReentrantLock();
 
     protected WinDriverService(File executable, int port, Duration timeout, List<String> args, Map<String, String> environment) throws IOException {
         super(executable, port, timeout, args, environment);
@@ -31,6 +47,54 @@ public class WinDriverService extends DriverService {
     @Override
     public String getDriverProperty() {
         return WINDRIVER_EXE_PROPERTY;
+    }
+
+    @Override
+    public boolean isRunning() {
+        lock.lock();
+        try {
+          return super.isRunning();
+        } finally {
+          lock.unlock();
+        }
+    }
+
+    @Override
+    public void start() throws IOException {
+        lock.lock();
+
+        if (isRunning()) return;
+        try {
+            if (getExecutable() == null) setExecutable(DriverFinder.getPath(this, getDefaultDriverOptions()).getDriverPath());
+            
+            process = new CommandLine(getExecutable(), getArgs().toArray(new String[0]));
+            process.setEnvironmentVariables(getEnvironment());
+            process.copyOutputTo(getOutputStream());
+            process.executeAsync();
+
+            CompletableFuture<Void> serverStarted = CompletableFuture.runAsync(() -> {
+                waitUntilAvailable();
+            }, waitAvailableExecutor);
+
+            try {
+                serverStarted.get(getTimeout().toMillis(), TimeUnit.MILLISECONDS);
+            } catch (Exception e) {
+                throw new WebDriverException("Timed out waiting for driver server to start.", e);
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public void stop() {
+        lock.lock();
+
+        try {
+            super.stop();
+        } finally {
+            lock.unlock();
+        }
     }
 
     @AutoService(DriverService.Builder.class)
